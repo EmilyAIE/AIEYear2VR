6e74b705b121db15c967780dd382733bd0a3f720
TobyAIE <126209021+TobyAIE@users.noreply.github.com>
2024-06-24 14:52:54 +0930

    Update(Prefab) : Updated names of prefabs

--- Perplexing Hexing/Assets/Ingredients/Scripts/Ingredient.cs
+++ Perplexing Hexing/Assets/Ingredients/Scripts/Ingredient.cs
@@ -5,5 +5,29 @@ using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
-    public string Name;    
+    public string Name;
+
+    [SerializeField]
+    float m_deleteTime;    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "BadSurface")
+        {
+            Invoke("DestroyIngredient", m_deleteTime);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "BadSurface")
+        {
+            CancelInvoke("DestroyIngredient");
+        }
+    }
+
+    void DestroyIngredient()
+    {
+        Destroy(gameObject);
+    }
 }
