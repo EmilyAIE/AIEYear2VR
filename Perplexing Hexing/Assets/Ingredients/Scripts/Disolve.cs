1e1d4c26fc2b3e5be65a748abc12bd9535200d4b
DanielDCruzAIE <128771290+DanielDCruzAIE@users.noreply.github.com>
2024-06-25 12:36:49 +0930

    Worked on Cauldron Liquid With Jackson

--- Perplexing Hexing/Assets/Ingredients/Scripts/Disolve.cs
+++ Perplexing Hexing/Assets/Ingredients/Scripts/Disolve.cs
@@ -5,25 +5,30 @@ using Pixelplacement;
 
 public class Disolve : MonoBehaviour
 {
-    private Material mat;
-    public AnimationCurve DisolveTween;
-    public Vector2 DisolveRange;
+    public AnimationCurve disolveInTween, disolveOutTween;
 
-    // Start is called before the first frame update
+    private Material mat;
+    private ParticleSystem particles;
+    private Vector2 DisolveRange = new(0, 1.5f);
+    private float disolveDuration = 2f;
+
     private void OnEnable()
     {
         mat = GetComponent<Renderer>().material;
+        particles = GetComponent<ParticleSystem>();
         DisolveIn();
     }
 
     public void DisolveIn()
     {
-        Tween.ShaderFloat(mat, "_Cutoff_Height", DisolveRange.y, 1, 0, DisolveTween);
+        Tween.ShaderFloat(mat, "_Cutoff_Height", DisolveRange.y, disolveDuration, 0, disolveInTween);
+        particles.Play();
     }
 
     public void DisolveOut()
     {
-        Tween.ShaderFloat(mat, "_Cutoff_Height", DisolveRange.x, 1, 0, DisolveTween);
+        Tween.ShaderFloat(mat, "_Cutoff_Height", DisolveRange.x, disolveDuration, 0, disolveOutTween);
+        particles.Play();
     }
 
     private void Update()
@@ -31,6 +36,11 @@ public class Disolve : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             DisolveOut();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            DisolveIn();
         }
     }
 
