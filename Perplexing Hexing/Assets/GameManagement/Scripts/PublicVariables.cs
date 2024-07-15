348a1f9533558b64bd8d87b37a8cd2a0fbc10eb6
AndrewAIE <126208967+AndrewAIE@users.noreply.github.com>
2024-07-15 11:29:20 +0930

    Many things
    Imported models and textures for Room, created spawners for potions,

---
+++ Perplexing Hexing/Assets/GameManagement/Scripts/PublicVariables.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CookState
+{
+    underCooked,
+    light,
+    medium,
+    wellDone,
+    overCooked
+}
